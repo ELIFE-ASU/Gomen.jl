1b79a2b4432d0990ab331e9bfc519b274fe9bad7
Douglas G. Moore <doug@dglmoore.com>
2019-10-11 23:47:48 -0700

    :hammer: Create Game struct and play function

--- src/Gomen.jl
+++ src/Gomen.jl
@@ -1,5 +1,37 @@
 module Gomen
 
-greet() = print("Hello World!")
+export Game, play
+
+using StaticArrays
+
+"""
+    Game(s, t)
+
+A two-player, two-strategy, symmetric game. The games are characterized by the diagonal elements of
+a payoff matrix
+
+``\\begin{bmatrix} 1 & S \\\\ T & 0 \\end{bmatrix}``
+
+with ``-0.5 \\leq S \\leq 0.5`` and ``0.5 \\leq T \\leq 1.5``,
+"""
+struct Game
+    payoffs::SMatrix{2, 2, Float64}
+    function Game(s::Number, t::Number)
+        if -0.5 ≤ s ≤ 0.5
+            if 0.5 ≤ t ≤ 1.5
+                return new([1.0 s; t 0.0])
+            end
+            throw(DomainError(t, "expected 0.5 ≤ t ≤ 1.5"))
+        end
+        throw(DomainError(s, "expected -0.5 ≤ s ≤ 0.5"))
+    end
+end
+
+"""
+    play(g::Game, a, b)
+
+Play the game with the strategies `a` and `b`, returning the payoff recieved by agent playing `a`.
+"""
+play(g::Game, a::Int, b::Int) = g.payoffs[a, b]
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,22 @@
-using Gomen, Test
+using Gomen, Test, StaticArrays
 
-@test 1 + 2 == 3
+@testset "Game" begin
+    @testset "Construction" begin
+        @test_throws DomainError Game(-1, 1)
+        @test_throws DomainError Game(1, 1)
+        @test_throws DomainError Game(0, 0)
+        @test_throws DomainError Game(0, 2)
+
+        @test Game(0.5, 1).payoffs == @SArray [1.0 0.5; 1.0 0.0]
+        @test Game(-0.3, 1.3).payoffs == @SArray [1.0 -0.3; 1.3 0.0]
+    end
+
+    @testset "Play" begin
+        let g = Game(-0.3, 1.3)
+            @test play(g, 1, 1) == 1.0
+            @test play(g, 1, 2) == -0.3
+            @test play(g, 2, 1) == 1.3
+            @test play(g, 2, 2) == 0.0
+        end
+    end
+end
