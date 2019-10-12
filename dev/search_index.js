6ad58ef846319073a92bf3771c2da1da316158a0
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-12 04:01:53 +0000

    build based on 319e170

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Gomen.jl-1","page":"Home","title":"Gomen.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Documentation for Gomen.jl","category":"page"}]
+}
