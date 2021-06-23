f4b9dc5ed92f6ebaa204f6bc9868f54058bf5b09
Douglas G. Moore <doug@dglmoore.com>
2021-06-23 16:09:18 -0700

    :hammer: Add and refactor graph generators

--- src/graphs.jl
+++ src/graphs.jl
@@ -1,66 +1,88 @@
 abstract type GraphGenerator end
 
-Base.length(g::GraphGenerator) = g.l
+Base.length(g::GraphGenerator) = g.count
+
+LightGraphs.nv(g::GraphGenerator) = g.nodes
+
+params(g::GraphGenerator) = tuple()
+
+Base.iterate(g::GraphGenerator) = if length(g) > 0
+    graph = generate(g)
+    while !is_connected(graph)
+        graph = generate(g)
+    end
+    graph, length(g)
+else
+    nothing
+end
+
+Base.iterate(g::GraphGenerator, i::Int) = if i > 1
+    graph = generate(g)
+    while !is_connected(graph)
+        graph = generate(g)
+    end
+    graph, i - 1
+else
+    nothing
+end
+
+struct CycleGraphGenerator <: GraphGenerator
+    nodes::Int
+end
+CycleGraphGenerator(; nodes::Int=1, kwargs...) = CycleGraphGenerator(nodes)
+
+Base.length(::CycleGraphGenerator) = 1
+
+generate(c::CycleGraphGenerator) = cycle_graph(c.nodes)
+
+struct WheelGraphGenerator <: GraphGenerator
+    nodes::Int
+end
+WheelGraphGenerator(; nodes::Int=1, kwargs...) = WheelGraphGenerator(nodes)
+
+Base.length(::WheelGraphGenerator) = 1
+
+generate(w::WheelGraphGenerator) = wheel_graph(w.nodes)
+
+struct StarGraphGenerator <: GraphGenerator
+    nodes::Int
+end
+StarGraphGenerator(; nodes::Int=1, kwargs...) = StarGraphGenerator(nodes)
+
+Base.length(::StarGraphGenerator) = 1
+
+generate(s::StarGraphGenerator) = star_graph(s.nodes)
+
+struct GridGraphGenerator <: GraphGenerator
+    dims::Vector{Int}
+    periodic::Bool
+end
+GridGraphGenerator(; dims::Vector{Int}=[1], periodic=true) = new(dims, periodic)
+
+Base.length(::GridGraphGenerator) = 1
+
+LightGraphs.nv(g::GridGraphGenerator) = prod(g.dims)
+
+generate(g::GridGraphGenerator) = grid(g.dims; periodic=g.periodic)
 
 struct BarabasiAlbertGenerator <: GraphGenerator
-    l::Int
-    n::Int
+    count::Int
+    nodes::Int
     k::Int
 end
+BarabasiAlbertGenerator(; count::Int=1, nodes::Int=1, k::Int=1, kwargs...) = BarabasiAlbertGenerator(count, nodes, k)
 
-LightGraphs.nv(ba::BarabasiAlbertGenerator) = ba.n
+params(ba::BarabasiAlbertGenerator) = (; k=ba.k)
 
-param(ba::BarabasiAlbertGenerator) = ba.k
-
-Base.iterate(ba::BarabasiAlbertGenerator) = if ba.l > 0
-    g = barabasi_albert(ba.n, ba.k)
-    while !is_connected(g)
-        barabasi_albert(ba.n, ba.k)
-    end
-    g, ba.l
-else
-    nothing
-end
-
-Base.iterate(ba::BarabasiAlbertGenerator, i::Int) = if i > 1
-    g = barabasi_albert(ba.n, ba.k)
-    while !is_connected(g)
-        barabasi_albert(ba.n, ba.k)
-    end
-    g, i - 1
-else
-    nothing
-end
+generate(ba::BarabasiAlbertGenerator) = barabasi_albert(ba.nodes, ba.k)
 
 struct ErdosRenyiGenerator <: GraphGenerator
-    l::Int
-    n::Int
+    count::Int
+    nodes::Int
     p::Float64
 end
+ErdosRenyiGenerator(; count::Int=1, nodes::Int=1, p::Float64=0.5, kwargs...) = ErdosRenyiGenerator(count, nodes, p)
 
-LightGraphs.nv(er::ErdosRenyiGenerator) = er.n
+params(er::ErdosRenyiGenerator) = (; p=er.p)
 
-param(er::ErdosRenyiGenerator) = er.p
-
-Base.iterate(er::ErdosRenyiGenerator) = if er.l > 0
-    g = erdos_renyi(er.n, er.p)
-    while !is_connected(g)
-        g = erdos_renyi(er.n, er.p)
-    end
-    g, er.l
-else
-    nothing
-end
-
-Base.iterate(er::ErdosRenyiGenerator, i::Int) = if i > 1
-    g = erdos_renyi(er.n, er.p)
-    while !is_connected(g)
-        g = erdos_renyi(er.n, er.p)
-    end
-    g, i - 1
-else
-    nothing
-end
-
-Base.length(er::ErdosRenyiGenerator) = er.l
-
+generate(er::ErdosRenyiGenerator) = erdos_renyi(er.nodes, er.p)
